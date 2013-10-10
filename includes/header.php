fe1c9fddcb45338b0c1136972360d15305af7689
Webster Massingham <wmassingham@gmail.com>
2013-10-10 12:03:38 -0400

    add dev header

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,7 @@
 <?php
   echo '<title>Assessment - ' . $page_title . '</title>';
 ?>
-  <link rel="icon" type="image/png" href="images/favicon.png" /> 
+  <link rel="icon" type="image/png" href="images/favicon.png" />
 
   <link rel="stylesheet" type="text/css" href="css/jquery-ui.css" />
   <link rel="stylesheet" type="text/css" href="css/tabzilla.css" />
@@ -17,7 +17,7 @@
     <!--<link rel="stylesheet" type="text/css" href="css/dataTables_themeroller.css" />-->
 </head>
 <body>
-    
+
     <div id="spacer">&nbsp;</div>
     <div id="header">
         <div class="headerContainer">
@@ -25,16 +25,17 @@
 		<a href="http://www.keene.edu"><img src="images/KSC-wordmark-150px.png" alt="Keene State College" /></a>
 		<div id="logoTitleDiv">
 			<h2 id="djcWEMlib" ><a href="http://keene.edu/academics/library/">Wallace E. Mason Library</a></h2>
-			<h2 id="assessmentTitle"><a href="index.php">Information Literacy Assessment</a></h2>
-		</div></div> </div> 
+			<!--<h2 id="assessmentTitle"><a href="index.php">Information Literacy Assessment</a></h2>-->
+			<h2 id="assessmentTitle" ><a style="color: #1DC623;" href="index.php">** Development Copy **</a></h2>
+		</div></div> </div>
 <?php
- // echo '<div id="pageTitleDiv"><h3>Assessment - ' . $page_title . '</h3></div>'; 
+ // echo '<div id="pageTitleDiv"><h3>Assessment - ' . $page_title . '</h3></div>';
   echo '<div id="loginDiv">';
   /*IF Logged in then tell the user and offer logout */ ?>
         <h3 id="title" title="Current Location:"><?php  echo $page_title; ?></h3>
         <?PHP
    if (isset($_SESSION['userID']))
-       { 
+       {
        echo('<span id="loginLine" class="login">You are logged in as <strong>' . $_SESSION['userName'] . '.</strong></span>');
        include('logoutForm.php');
        }
@@ -42,15 +43,14 @@
     <br />
     </div><!--loginDiv-->
     <!-- <a href="http://www.mozilla.org/" id="tabzilla">mozilla</a> -->
-   
-    <?php 
+
+    <?php
        // include('topNavigation.php');
        // if($page_title!='Home'){include('assessmentMenu.php');} ?>
-    
-   
+
+
         <div id="slider" class="open"><?PHP include('includes/tabzillaMenu1.php') ?></div> <a href="index.php" id="tabzilla">mozilla</a>
     </div><!-- header-->
     <div id="content">
-      
-    
-    
+
+
