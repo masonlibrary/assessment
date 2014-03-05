5b3430965aaa610fef680497ca477002e6a5dc89
Webster Massingham <wmassingham@gmail.com>
2014-03-05 15:06:36 -0500

    disable editing of existing outcome headings/details

--- outcomeManage.php
+++ outcomeManage.php
@@ -26,25 +26,24 @@
 			$headingActive='no';
 		}
 		
-		$stmt = mysqli_prepare($dbc, 'update outcomeheading set otchName=?, otchActive=? where otchID=?');
-		mysqli_bind_param($stmt, "ssi", $_POST['outcomeHeading'], $headingActive, $_POST['headingID']);
-//		echo 'update outcomeheading set otchName='.$_POST['outcomeHeading'].', otchActive='.$headingActive.' where otchID='.$_POST['headingID'];
+//		$stmt = mysqli_prepare($dbc, 'update outcomeheading set otchName=?, otchActive=? where otchID=?');
+//		mysqli_bind_param($stmt, "ssi", $_POST['outcomeHeading'], $headingActive, $_POST['headingID']);
+		$stmt = mysqli_prepare($dbc, 'update outcomeheading set otchActive=? where otchID=?');
+		mysqli_bind_param($stmt, "si", $headingActive, $_POST['headingID']);
 		mysqli_stmt_execute($stmt) or die('Failed to update outcome headings: ' . mysqli_error($dbc));
 		
-		$stmt = mysqli_prepare($dbc, 'update outcomedetail set otcdName=? where otcdID=?');
-		foreach ($_POST['editOutcomeDetailName'] as $i=>$name) {
-			if (!$name) { $name = "(blank)"; }
-			mysqli_bind_param($stmt, "si", $name, $i);
-//			echo 'update outcomedetail set otcdName="'.$name.'" where otcdID='.$i.'<br/>';
-			mysqli_stmt_execute($stmt) or die('Failed to execute query: ' . mysqli_error($dbc));
-		}
+//		$stmt = mysqli_prepare($dbc, 'update outcomedetail set otcdName=? where otcdID=?');
+//		foreach ($_POST['editOutcomeDetailName'] as $i=>$name) {
+//			if (!$name) { $name = "(blank)"; }
+//			mysqli_bind_param($stmt, "si", $name, $i);
+//			mysqli_stmt_execute($stmt) or die('Failed to update outcome detail names: ' . mysqli_error($dbc));
+//		}
 		
 		$stmt = mysqli_prepare($dbc, 'insert into outcomedetail (otcdotchID, otcdName) values (?, ?)');
 		foreach ($_POST['newOutcomeDetailName'] as $name) {
 			if(!$name) { continue; }
 			mysqli_bind_param($stmt, "is", $id, $name);
-//			echo 'insert into outcomedetail (otcdotchID, otcdName) values ('.$id.', '.$name.')<br/>';
-			mysqli_stmt_execute($stmt) or die('Failed to execute query: ' . mysqli_error($dbc));
+			mysqli_stmt_execute($stmt) or die('Failed to insert new outcome details: ' . mysqli_error($dbc));
 		}
 		
 		header('Location: outcomeList.php');
@@ -62,14 +61,10 @@
 //	mysqli_stmt_free_result($stmt);
 	
 	echo '<h1>'.$page_title.'</h1>
-		<h2>'.$heading['otchName'].'</h2>
-		';
-	
-	echo '<form method="post">
-		<div class="item" style="width:initial">
-		<h3>Heading name</h3>
-		<textarea name="outcomeHeading" style="width:100%">'.$heading['otchName'].'</textarea>
-		<h3>Edit existing outcome details</h3>
+		<form method="post">
+		<h2>Outcome heading name</h2>
+		<textarea name="outcomeHeading" style="width:100%" disabled>'.$heading['otchName'].'</textarea>
+		<h2>Existing outcome details</h2>
 		<table style="width:100%">';
 	
 	$stmt = mysqli_prepare($dbc, 'select otcdID, otcdotchID, otcdName from outcomedetail where otcdotchID=? order by otcdName');
@@ -78,12 +73,12 @@
 	mysqli_stmt_store_result($stmt);
 	mysqli_stmt_bind_result($stmt, $detail['otcdID'], $detail['otcdotchID'], $detail['otcdName']);
 	while (mysqli_stmt_fetch($stmt)) {
-		echo '<tr><td><textarea name="editOutcomeDetailName['.$detail['otcdID'].']" style="width:100%">'.htmlspecialchars($detail['otcdName']).'</textarea></td></tr>';
+		echo '<tr><td><textarea name="editOutcomeDetailName['.$detail['otcdID'].']" style="width:100%" disabled>'.htmlspecialchars($detail['otcdName']).'</textarea></td></tr>';
 	}
 	mysqli_stmt_free_result($stmt);
 	
 	echo '</table>
-	 <h3>Add new outcome details</h3>
+	 <h2>New outcome details</h2>
 	 <table style="width:100%">';
 	
 	for ($i=0; $i<6; $i++) {
