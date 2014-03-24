e8996009ee0540462a653d56ac96a35209dce7d1
Webster Massingham <wmassingham@gmail.com>
2014-03-24 15:10:37 -0400

    inline singleton getMySessions() function to allow more dataTables functionality remove now-unused getMySessions() from classes/User.php

--- classes/User.php
+++ classes/User.php
@@ -189,62 +189,6 @@ class User
 		return $output;
 	}
 
-	public function getMySessions($inID)
-				{
-            $dbc=$this->getConnection();
-
-						$output = '<table id="mySessions"><thead id="mySessionsHead"><tr>
-							<th>Course</th>
-							<th>Title</th>
-							<th>Faculty</th>
-							<th>Session</th>
-							<th>Semester</th>
-							<th>Date</th>
-							<th>Date2</th>
-							<th>Outcomes</th>
-							<th>Assessed</th>
-							<th>Tools</th>
-							</tr></thead><tbody>';
-						$query = 'select 
-								s.sesdID as sessionID, 
-								cp.crspName as CoursePrefix, 
-								s.sesdCourseNumber as CourseNumber, 
-								s.sesdCourseSection as CourseSection, 
-								s.sesdCourseTitle as CourseTitle, 
-								s.sesdSessionSection as SessionNum, 
-								s.sesdDate as Date, 
-								s.sesdFaculty as Faculty, 
-								s.sesdOutcomeDone as OutcomeDone, 
-								s.sesdAssessed as AssessedDone 
-							from sessiondesc s, courseprefix cp 
-							where s.sesdlibmID=? and s.sesdcrspID=cp.crspID 
-							order by CoursePrefix, CourseNumber, CourseSection';
-
-						$stmt = mysqli_prepare($dbc, $query);
-						mysqli_bind_param($stmt, 'i', $inID);
-						mysqli_stmt_execute($stmt) or die('Failed to retrieve session info: ' . mysqli_error($dbc));
-						mysqli_stmt_store_result($stmt);
-						mysqli_stmt_bind_result($stmt, $sessionID, $coursePrefix, $courseNumber, $courseSection, $courseTitle, $sessionNumber, $date, $faculty, $outcomeDone, $AssessedDone);
-						while (mysqli_stmt_fetch($stmt)) {
-							$output.="<tr class='mySessions' id='$sessionID'>
-								<td class='coursePrefix'>$coursePrefix $courseNumber-$courseSection</td>
-								<td class='courseTitle' >$courseTitle</td>
-								<td>$faculty</td>
-								<td>$sessionNumber</td>
-								<td>".toSemester($date)."</td>
-								<td class='dateCell'>".toUSDate($date)."</td>
-								<td class='sqlDateCel'>$date</td>
-								<td>$outcomeDone</td>
-								<td>$AssessedDone</td>
-								<td><div id='d$sessionID' class='menu-div'><p>+</p></div></td>";
-						}
-						mysqli_stmt_free_result($stmt);
-
-						$output.='</tbody></table>';
-						return $output;
-							
-				}
-
      public function getNeedAssessed($inID) {
 				$dbc=$this->getConnection();
 
--- mySessions.php
+++ mySessions.php
@@ -1,86 +1,115 @@
 <?php
-   
-    include('control/connectionVars.php');
-    include_once('control/functions.php');
-    include('classes/InstructionSession.php');
-    include('classes/User.php');
+
+	include('control/connectionVars.php');
+	include_once('control/functions.php');
+	include('classes/InstructionSession.php');
+	include('classes/User.php');
 
 	require_once('control/startSession.php');
-    
-    
-    // Insert the page header
-  $page_title = 'My Sessions';
-  include('includes/header.php');
-  
-  $thisUser=$_SESSION['thisUser'];
-  
-  ?>
-  
 
-    
-<a href="#" class="test">Test: Chart filtered sessions by prefix</a> 
-        
-    <?php
-         if ($thisUser->isLibrarian)
-                { 
-                $_SESSION['currentLibrarianID']=$thisUser->getLibrarianID(); 
-                $output=$thisUser->getMySessions($_SESSION['currentLibrarianID']);
-                
-                
-                echo $output;
-                }
-   
-  
-  
-     ?>
-  
-  
-    
-  <div id="chartContainer" style="clear: both; margin-top: 40px;"> <div id="testChart"></div> </div>
-  
+	$page_title = 'My Sessions';
+	include('includes/header.php');
 
-  
-  <?php
+	$thisUser = $_SESSION['thisUser'];
 
-		$jsOutput .= 'var oTable = $("#mySessions").dataTable({
-				"sDom": "T<\'clear\'>lfrtip",
-				"iDisplayLength": -1,
-				"aLengthMenu": [[25, 50, 100, -1], [25, 50, 100, "All"]],
-				"aoColumns": [
-					null,
-					null,
-					null,
-					null,
-					null,
-					{"iDataSort": 6},
-					{"bVisible": false},
-					null,
-					null,
-					null
-				],
-				"oTableTools": {
-					"sSwfPath": "swf/copy_csv_xls_pdf.swf",
-					"aButtons": [
-						{
-							"sExtends": "csv",
-							"sButtonText": "Excel/CSV",
-							"mColumns": [ 0, 1, 2, 3, 4, 5, 7, 8 ]
-						},{
-							"sExtends": "pdf",
-							"sButtonText": "PDF",
-							"mColumns": [ 0, 1, 2, 3, 4, 5, 7, 8 ]
-						},{
-							"sExtends": "print",
-							"sButtonText": "Print",
-							"mColumns": [ 0, 1, 2, 3, 4, 5, 7, 8 ]
-						},{
-							"sExtends": "copy",
-							"sButtonText": "Copy",
-							"mColumns": [ 0, 1, 2, 3, 4, 5, 7, 8 ]
-						}
-					]
-				}
-			});';
+	echo '<h2>'.$page_title.'</h2>';
+	echo '<a href="#" class="test">Test: Chart filtered sessions by prefix</a>';
 
-		include('includes/reportsFooter.php');
-  ?>
+	$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die('Error connecting to the stupid database');
+
+	if ($thisUser->isLibrarian) {
+		echo '<table id="mySessions"><thead id="mySessionsHead"><tr>
+					<th>Course</th>
+					<th>Title</th>
+					<th>Faculty</th>
+					<th>Session</th>
+					<th>Semester</th>
+					<th>Date</th>
+					<th>Date2</th>
+					<th>Outcomes</th>
+					<th>Assessed</th>
+					<th>Tools</th>
+					</tr></thead><tbody>';
+		$query = 'select
+						s.sesdID as sessionID,
+						cp.crspName as CoursePrefix,
+						s.sesdCourseNumber as CourseNumber,
+						s.sesdCourseSection as CourseSection,
+						s.sesdCourseTitle as CourseTitle,
+						s.sesdSessionSection as SessionNum,
+						s.sesdDate as Date,
+						s.sesdFaculty as Faculty,
+						s.sesdOutcomeDone as OutcomeDone,
+						s.sesdAssessed as AssessedDone
+					from sessiondesc s, courseprefix cp
+					where s.sesdlibmID=? and s.sesdcrspID=cp.crspID
+					order by CoursePrefix, CourseNumber, CourseSection';
+
+		$stmt = mysqli_prepare($dbc, $query);
+		mysqli_bind_param($stmt, 'i', $thisUser->getLibrarianID());
+		mysqli_stmt_execute($stmt) or die('Failed to retrieve session info: ' . mysqli_error($dbc));
+		mysqli_stmt_store_result($stmt);
+		mysqli_stmt_bind_result($stmt, $sessionID, $coursePrefix, $courseNumber, $courseSection, $courseTitle, $sessionNumber, $date, $faculty, $outcomeDone, $AssessedDone);
+		while (mysqli_stmt_fetch($stmt)) {
+			echo "<tr class='mySessions' id='$sessionID'>
+											<td class='coursePrefix'>$coursePrefix $courseNumber-$courseSection</td>
+											<td class='courseTitle' >$courseTitle</td>
+											<td>$faculty</td>
+											<td>$sessionNumber</td>
+											<td>" . toSemester($date) . "</td>
+											<td class='dateCell'>" . toUSDate($date) . "</td>
+											<td class='sqlDateCel'>$date</td>
+											<td>$outcomeDone</td>
+											<td>$AssessedDone</td>
+											<td><div id='d$sessionID' class='menu-div'><p>+</p></div></td>";
+		}
+		mysqli_stmt_free_result($stmt);
+
+		echo '</tbody></table>';
+	}
+
+	echo '<div id="chartContainer" style="clear: both; margin-top: 40px;"> <div id="testChart"></div> </div>';
+
+	$jsOutput .= 'var oTable = $("#mySessions").dataTable({
+					"sDom": "T<\'clear\'>lfrtip",
+					"iDisplayLength": -1,
+					"aLengthMenu": [[25, 50, 100, -1], [25, 50, 100, "All"]],
+					"aoColumns": [
+						null,
+						null,
+						null,
+						null,
+						null,
+						{"iDataSort": 6},
+						{"bVisible": false},
+						null,
+						null,
+						null
+					],
+					"oTableTools": {
+						"sSwfPath": "swf/copy_csv_xls_pdf.swf",
+						"aButtons": [
+							{
+								"sExtends": "csv",
+								"sButtonText": "Excel/CSV",
+								"mColumns": [ 0, 1, 2, 3, 4, 5, 7, 8 ]
+							},{
+								"sExtends": "pdf",
+								"sButtonText": "PDF",
+								"mColumns": [ 0, 1, 2, 3, 4, 5, 7, 8 ]
+							},{
+								"sExtends": "print",
+								"sButtonText": "Print",
+								"mColumns": [ 0, 1, 2, 3, 4, 5, 7, 8 ]
+							},{
+								"sExtends": "copy",
+								"sButtonText": "Copy",
+								"mColumns": [ 0, 1, 2, 3, 4, 5, 7, 8 ]
+							}
+						]
+					}
+				});';
+
+	include('includes/reportsFooter.php');
+
+?>
