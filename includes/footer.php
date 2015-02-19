2d9ee7e853be7f201ff48102d05949eeced3676f
Webster Massingham <wmassingham@gmail.com>
2015-02-19 10:37:16 -0500

    reformat header/footer (no actual change)

--- includes/footer.php
+++ includes/footer.php
@@ -1,26 +1,26 @@
-</div>
-<div id="footer">
-    <?php echo 'Copyright &copy; 2013-' . date('Y') . ' Keene State College'; ?>
-</div>
+		</div>
+		<div id="footer">
+			<?php echo 'Copyright &copy; 2013-' . date('Y') . ' Keene State College'; ?>
+		</div>
 
-<script type="text/javascript" src="js/jquery.js"></script>
-<script type="text/javascript" src="js/jquery-ui.js"></script>
-<script type="text/javascript" src="js/jquery.ui.selectmenu.js"></script>
-<script type="text/javascript" src="js/dataTables.js"></script>
-<script type="text/javascript" src="js/dataTablesRowGrouping.js"></script>
-<script type="text/javascript" src="js/dataTables.fixedHeader.min.js"></script>
-<script type="text/javascript" src="js/TableTools.js"></script>
-<script type="text/javascript" src="js/ZeroClipboard.js"></script>
-<!--<script type="text/javascript" src="js/highcharts.js"></script>-->
-<!--<script type="text/javascript" src="js/exporting.js"></script>-->
-<script type="text/javascript" src="//code.highcharts.com/4.0/highcharts.js"></script>
-<script type="text/javascript" src="//code.highcharts.com/4.0/modules/exporting.js"></script>
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/jquery-ui.js"></script>
+		<script type="text/javascript" src="js/jquery.ui.selectmenu.js"></script>
+		<script type="text/javascript" src="js/dataTables.js"></script>
+		<script type="text/javascript" src="js/dataTablesRowGrouping.js"></script>
+		<script type="text/javascript" src="js/dataTables.fixedHeader.min.js"></script>
+		<script type="text/javascript" src="js/TableTools.js"></script>
+		<script type="text/javascript" src="js/ZeroClipboard.js"></script>
+		<!--<script type="text/javascript" src="js/highcharts.js"></script>-->
+		<!--<script type="text/javascript" src="js/exporting.js"></script>-->
+		<script type="text/javascript" src="//code.highcharts.com/4.0/highcharts.js"></script>
+		<script type="text/javascript" src="//code.highcharts.com/4.0/modules/exporting.js"></script>
 
-<script type="text/javascript" src="js/sessionInput.js" ></script>
-<script type="text/javascript" src="js/pageStart.js" ></script>
-<script type="text/javascript" src="js/docReady.js" ></script>
+		<script type="text/javascript" src="js/sessionInput.js" ></script>
+		<script type="text/javascript" src="js/pageStart.js" ></script>
+		<script type="text/javascript" src="js/docReady.js" ></script>
 
-<?php if (isset($jsOutput)) { echo '<script type="text/javascript">'.$jsOutput.'</script>'; } ?>
+		<?php if (isset($jsOutput)) { echo '<script type="text/javascript">'.$jsOutput.'</script>'; } ?>
 
-</body>
+	</body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -9,71 +9,64 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-<?php
-  echo '<title>' . $page_title . ' - Assessment</title>';
-?>
-  <link rel="icon" type="image/png" href="images/favicon.png" />
+		<?php echo '<title>' . $page_title . ' - Assessment</title>'; ?>
+		<link rel="icon" type="image/png" href="images/favicon.png" />
 
-  <link rel="stylesheet" type="text/css" href="css/jquery-ui.css" />
-  <link rel="stylesheet" type="text/css" href="css/tabzilla.css" />
-    <link rel="stylesheet" type="text/css" href="css/assessment.css" />
-    <link rel="stylesheet" type="text/css" href="css/dataTables.css" />
-    <link rel="stylesheet" type="text/css" href="css/TableTools.css" />
-    <!--<link rel="stylesheet" type="text/css" href="css/dataTables_themeroller.css" />-->
-</head>
-<body>
+		<link rel="stylesheet" type="text/css" href="css/jquery-ui.css" />
+		<link rel="stylesheet" type="text/css" href="css/tabzilla.css" />
+		<link rel="stylesheet" type="text/css" href="css/assessment.css" />
+		<link rel="stylesheet" type="text/css" href="css/dataTables.css" />
+		<link rel="stylesheet" type="text/css" href="css/TableTools.css" />
+		<!--<link rel="stylesheet" type="text/css" href="css/dataTables_themeroller.css" />-->
+	</head>
+	<body>
+		<div id="header">
+			<div class="headerContainer">
+				<div class="logo-block">
+					<a href="http://www.keene.edu"><img src="images/KSC-wordmark-150px.png" alt="Keene State College" /></a>
+					<div id="logoTitleDiv">
+						<h2 id="djcWEMlib" ><a href="http://keene.edu/academics/library/">Wallace E. Mason Library</a></h2>
+						<?php
+							// Check to see if we're local or the path to this script starts with this:
+							// Same check is used in control/connectionVars.php
+							$practiceprefix = '/assessment/practice/assessment';
+							if ($_SERVER['HTTP_HOST'] == 'localhost' || (strncmp($_SERVER['SCRIPT_NAME'], $practiceprefix, strlen($practiceprefix)) == 0)) {
+								echo '<h2 id="assessmentTitle" ><a style="color:#2C2;" href="index.php">** Development Copy **</a></h2>';
+							} else {
+								echo '<h2 id="assessmentTitle" ><a href="index.php">Information Literacy Assessment</a></h2>';
+							}
+						?>
+					</div>
+				</div>
+			</div>
+			<div id="loginDiv">
+				<h3 id="title"><?php echo $page_title; ?></h3>
+				<?PHP
+					if (isset($_SESSION['userID'])) {
+						echo '<div id="loginLine" class="login">You are logged in as <strong>' . $_SESSION['userName'] . '</strong>.</div>';
+						echo '<div id="userlinks"><a href="userEdit.php">Account</a> &bull; <a href="logout.php">Logout</a></div>';
+					}
+				?>
+				<br />
+			</div><!--loginDiv-->
 
-    <div id="header">
-        <div class="headerContainer">
-       <div class="logo-block">
-		<a href="http://www.keene.edu"><img src="images/KSC-wordmark-150px.png" alt="Keene State College" /></a>
-		<div id="logoTitleDiv">
-			<h2 id="djcWEMlib" ><a href="http://keene.edu/academics/library/">Wallace E. Mason Library</a></h2>
-			<?php
-				// Check to see if we're local or the path to this script starts with this:
-				// Same check is used in control/connectionVars.php
-				$practiceprefix='/assessment/practice/assessment';
-				if ($_SERVER['HTTP_HOST'] == 'localhost' || (strncmp($_SERVER['SCRIPT_NAME'], $practiceprefix, strlen($practiceprefix)) == 0)) {
-					echo '<h2 id="assessmentTitle" ><a style="color:#2C2;" href="index.php">** Development Copy **</a></h2>';
-				} else {
-					echo '<h2 id="assessmentTitle" ><a href="index.php">Information Literacy Assessment</a></h2>';
-				}
-			?>
-		</div></div> </div>
-<?php
- // echo '<div id="pageTitleDiv"><h3>Assessment - ' . $page_title . '</h3></div>';
-  echo '<div id="loginDiv">';
-  /*IF Logged in then tell the user and offer logout */ ?>
-        <h3 id="title" title="Current Location:"><?php  echo $page_title; ?></h3>
-        <?PHP
-   if (isset($_SESSION['userID']))
-       {
-       echo '<div id="loginLine" class="login">You are logged in as <strong>' . $_SESSION['userName'] . '</strong>.</div>';
-       echo '<div id="userlinks"><a href="userEdit.php">Account</a> &bull; <a href="logout.php">Logout</a></div>';
-       }
-?>
-    <br />
-    </div><!--loginDiv-->
-    <!-- <a href="http://www.mozilla.org/" id="tabzilla">mozilla</a> -->
-
-    <?php
-       // include('topNavigation.php');
-       // if($page_title!='Home'){include('assessmentMenu.php');} ?>
-
-
-        <div id="slider" class="closed"><?PHP include('includes/tabzillaMenu1.php') ?></div> <a href="#" id="tabzilla">mozilla</a>
-    </div><!-- header-->
-    <div id="content">
+			<div id="slider" class="closed"><?PHP include('includes/tabzillaMenu1.php') ?></div> <a href="#" id="tabzilla">mozilla</a>
+		</div><!-- header-->
+		<div id="content">
 
 		<?php
 			if ((isset($_SESSION['dialogTitle']) && $_SESSION['dialogTitle']) || (isset($_SESSION['dialogText']) && $_SESSION['dialogText'])) {
 				echo '<div id="messagebox" class="">';
 				echo '<a id="messagebox-close" class="right">&#10006;</a>';
-				if (isset($_SESSION['dialogTitle'])) { echo '<strong>' . $_SESSION['dialogTitle'] . '</strong><br/>'; }
-				if (isset($_SESSION['dialogText'])) { echo $_SESSION['dialogText']; }
+				if (isset($_SESSION['dialogTitle'])) {
+					echo '<strong>' . $_SESSION['dialogTitle'] . '</strong><br/>';
+				}
+				if (isset($_SESSION['dialogText'])) {
+					echo $_SESSION['dialogText'];
+				}
 				echo '</div>';
 			}
 			unset($_SESSION['dialogTitle']);
