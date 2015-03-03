6c7441a6b8f246d9ae3bfbaa8b47fc35f336d3a1
Webster Massingham <wmassingham@gmail.com>
2015-03-03 10:14:53 -0500

    set sql session timezone on connect

--- control/connection.php
+++ control/connection.php
@@ -2,4 +2,7 @@
 require_once('connectionVars.php');
 $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
                     or die('Error connecting to the stupid database');
+
+mysqli_query($dbc, 'set @@session.time_zone = "America/New_York"') or die('Failed to set SQL session timezone: ' . mysqli_error($dbc));
+
 ?>
