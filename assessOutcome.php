991837d924de772c30e464594f4f34126142f063
Webster Massingham <wmassingham@gmail.com>
2015-04-02 10:52:08 -0400

    support loading via $_GET var

--- assessOutcome.php
+++ assessOutcome.php
@@ -11,9 +11,18 @@ require_once('control/startSession.php');
   include('includes/header.php');
   
   echo '<h3 class="pageTitle">'.$page_title.'</h3>';
+
+	$id = 0;
+	if (isset($_POST['assessID'])) {
+		$id = $_POST['assessID'];
+	} else if (isset($_GET['session'])) {
+		$id = $_GET['session'];
+	} else {
+		die('no id!');
+	}
  
  $currentSession= new InstructionSession();
- $currentSession->loadSession($_POST['assessID']);
+ $currentSession->loadSession($id);
  
  echo $currentSession->getOutcomesToAssess();
 
